510e4768eded6cb1846467117d127479328b90b1
gvielza <80008338+gvielza@users.noreply.github.com>
2022-07-29 13:43:45 -0300

    agregado roles y spring security configurado

--- src/main/java/obspringsecurity/HelloController.java
+++ src/main/java/obspringsecurity/HelloController.java
@@ -5,8 +5,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @GetMapping
+    @GetMapping("/hi")
     public String hola(){
         return "Helloo People!";
+    }
+    @GetMapping("/hola")
+    public String holao(){
+        return "Helloo People desde hola2!";
     }
 }
